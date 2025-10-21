7dc5b6b8f9407321461f22e9e39176dad538dd15
Vivian Zitek <vivian.zitek@gmail.com>
2025-10-21 08:14:24 -0600

    Excluse .gitignore from project

--- obj/Debug/net8.0-windows/WinForms_Sandbox.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WinForms_Sandbox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinForms_Sandbox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32702b6cf2f11624af315be5bc93915fcde89f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0beaac743b1ab33f5b7c83ae73bbb896924d3df7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinForms_Sandbox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinForms_Sandbox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

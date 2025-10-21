2f1f74fba4c9eca77810041fd7372c2e8f2809cd
Vivian Zitek <vivian.zitek@gmail.com>
2025-10-21 08:45:54 -0600

    Add KeyDown function to text box to activate button when Enter is pressed

--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,28 @@
 namespace WinForms_Sandbox
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void buttonAdd_Click(object sender, EventArgs e)
-        {
-            if (!string.IsNullOrWhiteSpace(inputTextBox.Text) && !outputList.Items.Contains(inputTextBox.Text)) {
-                outputList.Items.Add(inputTextBox.Text);
-            }
-        }
-    }
+		private void buttonAdd_Click(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrWhiteSpace(inputTextBox.Text) && !outputList.Items.Contains(inputTextBox.Text))
+			{
+				outputList.Items.Add(inputTextBox.Text);
+			}
+		}
+
+
+		private void inputTextBox_KeyDown(object sender, KeyEventArgs e)
+		{
+
+			if (e.KeyCode == Keys.Enter) {
+				buttonAdd.PerformClick();
+			}
+		}
+
+	}
 }
--- obj/Debug/net8.0-windows/WinForms_Sandbox.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/WinForms_Sandbox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinForms_Sandbox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9779222d5a388f933eb3ce035d2453f0e6d6edc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57d0bc91026aae2b437dcc85ca7f798b0a01ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinForms_Sandbox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinForms_Sandbox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
